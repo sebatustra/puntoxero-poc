64724a65e7ecd64e9068b5d230e39d125398cb7d
sebatustra <sebatustra@protonmail.com>
2024-11-27 15:13:37 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use axum::{routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/hello-world", get(hello_world));
+
+    Ok(router.into())
+}
